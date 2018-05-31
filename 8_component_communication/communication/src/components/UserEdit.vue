411b0541870accdee3a4bf49199dbb271c3428ec
Emmanuel Lodovice <name3anad@gmail.com>
2018-06-01 02:35:41 +0800

    Moving logic to the bus

--- 8_component_communication/communication/src/components/UserEdit.vue
+++ 8_component_communication/communication/src/components/UserEdit.vue
@@ -16,7 +16,7 @@
             editAge() {
                 this.userAge = 30;
                 // this.$emit('ageWasEdited', this.userAge);
-                eventBus.$emit('ageWasEdited', this.userAge);
+                eventBus.changeAge(this.userAge);
             }
         }
     }
