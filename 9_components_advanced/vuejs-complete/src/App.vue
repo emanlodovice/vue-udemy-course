df0986d7a6f2ab29db630553136f7973fbdca1d7
Emmanuel Lodovice <name3anad@gmail.com>
2019-04-23 00:46:43 +0800

    Slots

---
+++ 9_components_advanced/vuejs-complete/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <button @click="selectedComponent = 'appQuote'">Quote</button>
+        <button @click="selectedComponent = 'appAuthor'">Author</button>
+        <button @click="selectedComponent = 'appNew'">New</button>
+        {{ selectedComponent }}
+        <hr>
+        <app-quote>
+          <h2 slot="title">{{ quoteTitle }}</h2>
+          <p slot="content">A wonderful Quote</p>
+        </app-quote>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Quote from './components/Quote.vue';
+  import Author from './components/Author.vue';
+  import New from './components/New.vue';
+  export default {
+    name: 'app',
+    data () {
+      return {
+        quoteTitle: 'The Quote',
+        selectedComponent: 'appQuote'
+      }
+    },
+
+    components: {
+      appQuote: Quote,
+      appAuthor: Author,
+      appNew: New
+    }
+  };
+</script>
+
+<style>
+  
+</style>
