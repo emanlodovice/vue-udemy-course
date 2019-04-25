e8826f6569fd3f96b74381c891a8dec6e9952516
Emmanuel Lodovice <name3anad@gmail.com>
2019-04-26 02:10:42 +0800

    Filters and computed properties

---
+++ 13_filter_mixin/src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <h1>Filters & Mixins</h1>
+        <p>{{ text | toUppercase | to-lowercase }}</p>
+        <hr>
+        <input v-model="filterText">
+        <ul>
+          <li v-for="fruit in filteredFruits">{{ fruit }}</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      text: 'Hello there!',
+      fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
+      filterText: ''
+    }
+  },
+  filters: {
+    toUppercase(value) {
+      return value.toUpperCase();
+    }
+  },
+  computed: {
+    filteredFruits() {
+      return this.fruits.filter((element) => {
+        return element.match(this.filterText);
+      });
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
